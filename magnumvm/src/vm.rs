dcded23f80d91f5ed2b423f3fff0ef5e0c439046
Eric Schneider <u1643364@live.unc.edu>
2021-11-21 00:01:00 -0500

    more work

---
+++ magnumvm/src/vm.rs
@@ -0,0 +1,122 @@
+use std::{fs, io};
+use magnum_common::{OPCODE_LOADI_B, OPCODE_NOP};
+use std::io::Read;
+
+pub struct VM {
+	text: Vec<u32>,
+	data: Vec<u8>,
+	stack_start: usize,
+	readable_start: usize,
+	stack_base: usize,
+	program_counter: usize
+}
+
+impl VM {
+	pub fn load(path: &str) -> io::Result<VM> {
+		//let vm = VM { text: vec![], data: vec![], stack_base: 0};
+
+		let mut file = fs::File::open(path)?;
+
+		let mut file_sig = [0u8; 3];
+		file.by_ref().take(3).read(&mut file_sig)?;
+
+		if file_sig.ne("MVM".as_bytes()) {
+			panic!("File signature does not match!");
+		}
+
+		let mut version = [0u8];
+		file.by_ref().take(3).read(&mut version)?;
+
+		if version.ne(&[0]) {
+			panic!("Version does not match!");
+		}
+
+		let mut text_loc = [0u8; 8];
+		file.by_ref().take(8).read(&mut text_loc)?;
+
+		if usize::from_le_bytes(text_loc).ne(&0x35usize) {
+			panic!("Version does not match!");
+		}
+
+		let mut text_size = [0u8; 8];
+		file.by_ref().take(8).read(&mut text_size)?;
+		let text_size = usize::from_le_bytes(text_size);
+
+		if text_size % 4 != 0 {
+			panic!("Instructions must be consistently 32-bit!");
+		}
+
+		//let mut text = Vec::with_capacity(text_size);
+
+		let mut readonly_loc = [0u8; 8];
+		file.by_ref().take(8).read(&mut readonly_loc)?;
+		let readonly_loc = usize::from_le_bytes(readonly_loc);
+
+		let mut readonly_size = [0u8; 8];
+		file.by_ref().take(8).read(&mut readonly_size)?;
+		let readonly_size = usize::from_le_bytes(readonly_size);
+
+		let mut init_writable_loc = [0u8; 8];
+		file.by_ref().take(8).read(&mut init_writable_loc)?;
+		let init_writable_loc = usize::from_le_bytes(init_writable_loc);
+
+		let mut init_writable_size = [0u8; 8];
+		file.by_ref().take(8).read(&mut init_writable_size)?;
+		let init_writable_size = usize::from_le_bytes(init_writable_size);
+
+		let mut uninit_writable_size = [0u8; 8];
+		file.by_ref().take(8).read(&mut uninit_writable_size)?;
+		let uninit_writable_size = usize::from_le_bytes(uninit_writable_size);
+
+		let mut text = Vec::with_capacity(text_size);
+
+		for _ in 0..(text_size / 4) {
+			let mut instruction = [0u8; 4];
+			file.by_ref().take(4).read(&mut instruction)?;
+			text.push(u32::from_le_bytes(instruction));
+		}
+
+		let mut data = Vec::with_capacity(readonly_size + init_writable_size + uninit_writable_size);
+
+		for _ in 0..readonly_size {
+			let mut byte = [0u8];
+			file.by_ref().take(1).read(&mut byte)?;
+			data.push(u8::from_le_bytes(byte));
+		}
+
+		for _ in 0..init_writable_size {
+			let mut byte = [0u8];
+			file.by_ref().take(1).read(&mut byte)?;
+			data.push(u8::from_le_bytes(byte));
+		}
+
+		for _ in 0..uninit_writable_size {
+			data.push(0);
+		}
+
+		let stack_start = data.len();
+		let readable_start = readonly_size;
+
+		Ok(VM { text, data, stack_start, readable_start, stack_base: 0, program_counter: 0})
+	}
+
+	pub fn run(&mut self) -> Result<(), String> {
+		loop {
+			if self.program_counter >= self.text.len() {
+				return Ok(());
+			}
+
+			let inst = self.text[self.program_counter];
+			let opcode = (inst >> (8 * 3)) as u8;
+
+			match opcode {
+				OPCODE_NOP => {}
+				OPCODE_LOADI_B => {
+					let im = (inst & 0xFF) as u8;
+				}
+			}
+
+			self.program_counter += 1;
+		}
+	}
+}
